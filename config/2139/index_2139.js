edcafbf4c21672ade025960bbd404bd9a3ba3cae
Vincent Cailleaud <vincent.cailleaud@triskellsoftware.fr>
2023-11-24 09:30:01 +0100

    new password

--- config/2139/index_2139.js
+++ config/2139/index_2139.js
@@ -3,7 +3,7 @@ module.exports = {
 		server : 'sandbox-eu.triskellsoftware.com',
 		login : 'admin@GOUV_RM2021.fr',
 		token : '+68VAzlF6cNN/kNqCo10fZcND1Ll5pEz2ICKG9jBGlI=',
-		password : '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
+		password : 'f07ee00ed1546bcf400d7da4eb6519b33ea9ca6f34197465912410b9f08c03a7', //adm123FR - 8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918
 		parameters: {
 			dataobject_id: "18"
 		},
